07c05587be71f3b3b1c3dd8bfd4c0d6be6dae873
duttash <shouvik.dutta@gmail.com>
2017-01-02 11:51:23 -0500

    webservice code patch 3

---
+++ mainModule/src/main/java/org/home/MainClass.java
@@ -0,0 +1,13 @@
+package org.home;
+
+/**
+ * Created by Shouvik on 1/2/2017.
+ */
+public class MainClass {
+    private static Integer hit = 0;
+
+    public static void main(String[] args) {
+        hit++;
+        System.out.println("Hello " + hit);
+    }
+}
