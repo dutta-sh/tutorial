0cd7724608313a2a6cbdba483e812ee5742fbc05
duttash <shouvik.dutta@gmail.com>
2017-03-11 15:46:39 -0500

    added new module

---
+++ websocket/src/main/java/org/home/HelloMessage.java
@@ -0,0 +1,13 @@
+package org.home;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by Shouvik on 3/11/2017.
+ */
+@Data
+@AllArgsConstructor
+public class HelloMessage {
+    private String name;
+}
---
+++ websocket/src/main/java/org/home/MyHandler.java
@@ -0,0 +1,20 @@
+package org.home;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * Created by Shouvik on 3/11/2017.
+ */
+@Controller
+public class MyHandler {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+}
