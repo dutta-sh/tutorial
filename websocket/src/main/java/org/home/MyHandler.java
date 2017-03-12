d550c1c06cb976d577bdd82b695cbba308c48d5a
duttash <shouvik.dutta@gmail.com>
2017-03-12 11:08:25 -0400

    added new module

--- websocket/src/main/java/org/home/MyHandler.java
+++ websocket/src/main/java/org/home/MyHandler.java
@@ -4,7 +4,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-
 /**
  * Created by Shouvik on 3/11/2017.
  */
