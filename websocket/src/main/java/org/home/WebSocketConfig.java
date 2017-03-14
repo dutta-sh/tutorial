82793fa0c1b1f2d2fe9bc19d3c8dacb25c4e7dcf
duttash <shouvik.dutta@gmail.com>
2017-03-14 16:17:37 -0400

    added new module

---
+++ websocket/src/main/java/org/home/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package org.home;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        System.out.print("I am here");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+}
